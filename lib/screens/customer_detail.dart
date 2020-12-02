f72d631fb6ae88209e66f0aae459113742a15177
darsh97 <aravinddarshan13897@gmail.com>
2020-12-02 15:40:51 +0530

    added new file

---
+++ lib/screens/customer_detail.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CustomerDetailPage extends StatefulWidget {
+  @override
+  _CustomerDetailPageState createState() => _CustomerDetailPageState();
+}
+
+class _CustomerDetailPageState extends State<CustomerDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
